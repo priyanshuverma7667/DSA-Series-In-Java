3f48cc63c8d5d125d768813add87c90b4b9e0f9b
Priyanshu Kumar Verma <priyanshu76670@gmail.com>
2026-09-24 19:25:02 +0530

    Implementing the Built In LinkedList Data

---
+++ LinkedListPractice/LinkedListInBuild.java
@@ -0,0 +1,14 @@
+package LinkedListPractice;
+
+import java.util.LinkedList;
+
+public class LinkedListInBuild{
+    public static void main(String args[]){
+        LinkedList nums = new LinkedList();
+
+        nums.add(5);
+        nums.add(6);
+        
+        System.out.println(nums);
+    }
+}
